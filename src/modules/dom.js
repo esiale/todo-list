7a18086d5d327dcaa37e7ff5b09b1bc218e92ea5
esiale <blazejp0@gmail.com>
2021-07-06 18:15:08 +0200

    Added mobile menu

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -9,8 +9,10 @@ function renderLayout() {
     header.append(logo);
 
     const main = document.createElement("main");
+    main.textContent = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.";
 
     const nav = document.createElement("nav");
+    nav.className = "nav-closed";
         const inbox = document.createElement("div");
         inbox.textContent = "Inbox";
         const today = document.createElement("div");
@@ -19,7 +21,9 @@ function renderLayout() {
         week.textContent = "Week";
         const projects = document.createElement("div");
         projects.textContent = "Projects";  
-    nav.append(inbox, today, week, projects);
+        const addNew = document.createElement("div");
+        addNew.textContent = "Add New";  
+    nav.append(inbox, today, week, projects, addNew);
 
     const mobileNav = document.createElement("div");
     mobileNav.className = "mobile-nav";
@@ -30,9 +34,8 @@ function renderLayout() {
     hambIcon.className = "material-icons-round style";
     hambIcon.textContent = "menu";
     hamburgerButton.addEventListener("click", showNav);
-    hamburgerButton.addEventListener("click", rotateIcon);
     hamburgerButton.append(hambIcon);
-
+    
     const addButton = document.createElement("div");
     addButton.className = "add-button";
     const addIcon = document.createElement("span");
@@ -47,18 +50,10 @@ function renderLayout() {
 function showNav() {
     const nav = document.querySelector("nav");
 
-    if (window.getComputedStyle(nav).display === "none") {
-    nav.style.display = "flex";
+    if (nav.classList.contains("nav-closed")) {
+        nav.className = "nav-open";
     } else {
-        nav.style.display = "none";
-    }
-}
-
-function rotateIcon(e) {
-    if (this.style.transform === "rotate(90deg)") {
-    this.style.transform = "rotate(0deg)";
-    } else {
-        this.style.transform = "rotate(90deg)";
+        nav.className = "nav-closed";
     }
 }
 
